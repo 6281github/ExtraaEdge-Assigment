a01acd3a3b261e06648c6514f92e315ed85ff29c
6281github <96714816+6281github@users.noreply.github.com>
2022-05-19 12:59:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,50 @@
-import React from 'react'
-import '../styles/App.css';
-import { Wrapper } from '../context/userContext';
-import { Settings } from './Settings';
-import { Home } from './Home';
-const App = () => {
+import Image from "./Components/Image";
+import { useState, useEffect } from "react";
+import './App.css'
 
-  return (
-    <div id="main">
-      <Wrapper>
-        <Settings />
-        <Home />
-      </Wrapper>
+
+
+export default function App() {
+  const [details, setDetails] = useState([])
+  
+
+  let tags = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+  let fixes = tags.map(item => {
+    return 'abcd' + item;
+  })
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(result => { setDetails(result) })
+  }, [])
+
+  console.log(details[0])
+  
+  // const heartStyle ={
+  //   color: {color}
+  // }
+
+  return <>
+
+    <div className="showCards">
+      {
+        details.map((item, index) => {
+          console.log(item.name, item.email, item.phone, item.website)
+          let url = 'https://avatars.dicebear.com/v2/avataaars/' + fixes[index] + '.svg?options[mood][]=happy';
+          return <>
+
+            <Image key={index} url={url} details={item} />
+           
+
+
+          </>
+        })
+      }
+
     </div>
-  )
+
+
+  </>
+
+
+
 }
-
-
-export default App;
